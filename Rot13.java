b4faf86e810d8111c5cc17800e1fa1bc19e16c8d
alex <alexvictorb@gmail.com>
2024-09-19 17:16:51 +0200

    prueba

---
+++ Rot13.java
@@ -0,0 +1,16 @@
+public class Rot13{
+
+
+    public String xifraRot13 (String cadena){
+        
+        String cifrat = "";
+        String abecedari = "abcdefghijqlmnopqrst";
+
+        for(int i=0;i<cadena.length();i++){
+
+        }
+        
+        return cifrat;
+        
+    }
+}
