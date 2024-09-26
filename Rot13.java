fdcce8bb06e8e57d018679f464ddb12790bbc50c
alex <alexvictorb@gmail.com>
2024-09-26 17:09:27 +0200

    Acabat

--- Rot13.java
+++ Rot13.java
@@ -6,7 +6,7 @@ public class Rot13{
         
         String cifrat = "";
         
-
+    
         for(int i=0;i<cadena.length();i++){
             if(Character.isLetter(cadena.charAt(i))){
                 
@@ -49,7 +49,60 @@ public class Rot13{
         
     }
 
+    public static String desxifraRot13 (String cadena){
+
+        String descifrat="";
+
+
+        for(int i=0;i<cadena.length();i++){
+            if(Character.isLetter(cadena.charAt(i))){
+                
+                for(int x=0;x<abecedari.length();x++){
+                    if(cadena.charAt(i)==abecedari.charAt(x)){
+                        String reempl = "";
+                        if((x - 13)< 0){
+                            int dif = (x - 13) + abecedari.length();
+                            reempl = Character.toString(abecedari.charAt(dif));
+                        }
+                        else{
+                            reempl = Character.toString(abecedari.charAt(x - 13));
+                        }
+                        descifrat = descifrat + reempl;
+                    }
+                    else{
+                        
+                            if(cadena.charAt(i)==abecedarimajus.charAt(x)){
+                                String reemplx = "";
+                                if((x - 13)< 0){
+                                    int dif = (x - 13) + abecedarimajus.length();
+                                    reemplx = Character.toString(abecedarimajus.charAt(dif));
+                                }
+                                else{
+                                    reemplx = Character.toString(abecedarimajus.charAt(x - 13));
+                                }
+                                descifrat = descifrat + reemplx;
+                            }
+                        
+
+                    }
+                }
+
+            }
+            else{descifrat = descifrat + Character.toString(cadena.charAt(i));}
+            
+        }
+
+
+        return descifrat;
+    }
+
     public static void main (String[]args){
-        System.out.println(xifraRot13("Hola Como.Estas?"));
+
+        String prova = "Hola Como.Estas?";
+        String xifrat = xifraRot13(prova);
+
+        System.out.println("La cadena es: " + prova);
+        System.out.println("La cadena cifrada es: " + xifraRot13(prova));
+        System.out.println("La cadena descifrada es: " + desxifraRot13(xifrat));
     }
 }
