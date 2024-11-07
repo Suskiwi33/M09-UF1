43acfbc2b2b8b8777b2b38f82c5e7d06140eb70e
alex <alexvictorb@gmail.com>
2024-11-07 18:36:40 +0100

    Acabat

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,35 @@
+package iticbcn.xifratge;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class ClauPublica {
+
+    public KeyPair generaParellClausRSA() throws Exception {
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+
+    }
+
+    public byte[] xifraRSA(String msg, PublicKey clauPublica)throws Exception {
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, clauPublica);
+        return cipher.doFinal(msg.getBytes("UTF-8"));
+
+    }
+
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey ClauPrivada) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, ClauPrivada); 
+        byte[] decryptedBytes = cipher.doFinal(msgXifrat);
+        return new String(decryptedBytes, "UTF-8"); 
+    }
+
+}
